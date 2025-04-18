9879d54be0a09c8093042699750ea618313f8ae6
lfuis201 <lmamania@ulasalle.edu.pe>
2025-04-18 00:09:06 -0500

    update: routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("login", "routes/login.tsx"),
+  index("routes/login.tsx"), // Esto hace que "/" apunte a login
+
   route("dashboard", "routes/dashboard.tsx"),
 
 ] satisfies RouteConfig;
